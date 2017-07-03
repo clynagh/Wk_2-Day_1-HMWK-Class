7c351dd0f838d21e01e094fa02326ee3d9405308
Craig Lynagh <craiglynagh@gmail.com>
2017-07-03 23:11:36 +0100

    library setup update

--- library_spec.rb
+++ library_spec.rb
@@ -4,6 +4,29 @@ require_relative ("./library")
 
 class TestLibrary < MiniTest::Test
 
+  def setup
+
+    @book = { 
+      title: "lord_of_the_rings",
+      rental_details: { 
+       student_name: "Jeff", 
+       date: "01/12/16"
+       },
+       { 
+        title: "lord_of_the_flies",
+        rental_details: { 
+         student_name: "Mary", 
+         date: "01/01/17"
+         },
+         { 
+          title: "lord_of_the_dance",
+          rental_details: { 
+           student_name: "Simon", 
+           date: "23/12/16"
+         }
+       }
+
+
 
   def test_library
     library = Library.new()
